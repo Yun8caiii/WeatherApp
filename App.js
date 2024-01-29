4ea76bb7993652aafadfd2a34e897eee3424cc70
Yun Cai (Jacky) <yun8caiii@gmail.com>
2024-01-29 00:52:05 -0800

    added states, hooks, loading states, fetched the weather api data, got the user location

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { ActivityIndicator, StyleSheet, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
+import * as Location from 'expo-location'
+import { WEATHER_API_KEY } from '@env'
+import { useGetWeather } from './src/hooks/useGetWeather'
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
+  const [loading, error, weather] = useGetWeather()
+  console.log(loading, error, weather)
 
-  )
-}
+  if (weather && weather.list) {  // if the weather exist with the data we'll render the weather otherwise show the activity indicator 
+    return (
+      <NavigationContainer>
+        <Tabs weather={weather}/>
+      </NavigationContainer>  
+    )
+  }
 
+
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'blue'} />
+      </View>
+    )
+  }
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+
+  }
+})
 
 export default App
