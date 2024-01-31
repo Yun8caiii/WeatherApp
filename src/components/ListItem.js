50b466591557792ded46ccf13d6734294ae53814
Yun Cai (Jacky) <yun8caiii@gmail.com>
2024-01-30 16:58:56 -0800

    added library moment.js and used changed the formating for date and time using moment librarys format

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,15 +1,19 @@
 import React from "react"
 import { View, Text, StyleSheet } from 'react-native'
 import { Feather } from '@expo/vector-icons'
+import moment from "moment"
 import { weatherType } from "../utilities/weatherType"
 
 const ListItem = (props) => {
     const { dt_txt, min, max, condition } = props   // destructuring 
-    const  { item, date, temp } = styles
+    const  { item, date, temp, dateTextWrapper } = styles
     return (
         <View style={item}>
             <Feather name={weatherType[condition].icon} size={50} color={'white'}/>
-            <Text style={date}>{dt_txt}</Text>
+            <View style={dateTextWrapper}>
+                <Text style={date}>{moment(dt_txt).format('dddd')}</Text>
+                <Text style={date}>{moment(dt_txt).format('h:mm:ss a')}</Text>
+            </View>
             <Text style={temp}>{`${Math.round(min)}°/${Math.round(max)}°`}</Text>
             
         </View>
@@ -34,6 +38,9 @@ const styles = StyleSheet.create({
     date: {
         color: 'white',
         fontSize: 15
+    },
+    dateTextWrapper: {
+        flexDirection: 'column'
     }
 })
 export default ListItem
